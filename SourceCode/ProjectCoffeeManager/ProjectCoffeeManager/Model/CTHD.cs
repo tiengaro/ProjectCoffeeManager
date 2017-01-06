134422845054c9254c5c5c6ac94848c80256826f
Kimkim <maserati9386@gmail.com>
2017-01-06 20:28:10 +0700

    Thêm Chi Tiet Hoa Don trong Model

---
+++ SourceCode/ProjectCoffeeManager/ProjectCoffeeManager/Model/CTHD.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectCoffeeManager.Model
+{
+    public class CTHD
+    {
+        string MaSp;
+        string TenSp;
+        string LoaiSp;
+        string GiaThanh;
+
+        public string MaSp1
+        {
+            get
+            {
+                return MaSp;
+            }
+
+            set
+            {
+                MaSp = value;
+            }
+        }
+
+        public string TenSp1
+        {
+            get
+            {
+                return TenSp;
+            }
+
+            set
+            {
+                TenSp = value;
+            }
+        }
+
+        public string LoaiSp1
+        {
+            get
+            {
+                return LoaiSp;
+            }
+
+            set
+            {
+                LoaiSp = value;
+            }
+        }
+
+        public string GiaThanh1
+        {
+            get
+            {
+                return GiaThanh;
+            }
+
+            set
+            {
+                GiaThanh = value;
+            }
+        }
+    }
+}
