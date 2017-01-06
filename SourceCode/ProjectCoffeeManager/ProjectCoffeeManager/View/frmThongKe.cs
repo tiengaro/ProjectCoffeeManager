ce7dcbaf78ce1d4b22eab272a2f5523514f3f84d
mrduc9669 <mrduc9669@gmail.com>
2017-01-06 11:53:53 +0700

    update ham xu ly su kien khi nguoi dung chon ngay trong form thong ke

--- SourceCode/ProjectCoffeeManager/ProjectCoffeeManager/View/frmThongKe.cs
+++ SourceCode/ProjectCoffeeManager/ProjectCoffeeManager/View/frmThongKe.cs
@@ -17,6 +17,27 @@ namespace ProjectCoffeeManager.View
             InitializeComponent();
         }
 
+        private void datetime_ValueChanged(object sender, EventArgs e)
+        {
+            //Lấy ngày tháng năm
+            string ngay = datetime.Value.Day.ToString();
+            string thang = datetime.Value.Month.ToString();
+            string nam = datetime.Value.Year.ToString();
+            //Xóa item trong listview thống kê
+            lstThongKe.Items.Clear();
+            DataTable dt = Control.ControlChiTietHoaDon.getChiTietHoaDonbyNgay(ngay, thang, nam);
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                ListViewItem item = new ListViewItem(dt.Rows[i].ItemArray[0].ToString());
+                item.SubItems.Add(dt.Rows[i].ItemArray[1].ToString());
+                item.SubItems.Add(dt.Rows[i].ItemArray[2].ToString());
+                item.SubItems.Add(dt.Rows[i].ItemArray[3].ToString());
+                item.SubItems.Add(dt.Rows[i].ItemArray[4].ToString());
+
+                lstThongKe.Items.Add(item);
+            }
+            txtTongTien.Text = Control.ControlChiTietHoaDon.getTongTienbyNgay(ngay, thang, nam);
+        }
         private void frmThongKe_Load(object sender, EventArgs e)
         {
 
